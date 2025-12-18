6b092764cee0c9891614d35ec7ae0d02ce88bc1d
francesco1203 <francesco.schettini12@gmail.com>
2025-12-18 19:29:15 +0100

    Aggiunta identificazione in spazio di stato

--- Matlab/functions/gen_PRBS.m
+++ Matlab/functions/gen_PRBS.m
@@ -1,21 +1,31 @@
 % Genera PRBS desiderato
 
-function generated_signal_PRBS = gen_PRBS(DeltaT, N_sim, amp_PRBS, offset_PRBS)
+function generated_signal_PRBS = gen_PRBS(DeltaT, N_sim, amp_PRBS, offset_PRBS, seed)
 
     % Input:
     %   DeltaT       - Passo di campionamento (in secondi) utilizzato nella simulazione.
     %   N_sim        - campioni di simulazione
     %   amp_PRBS     - Ampiezza massima del segnale PRBS (es. 2.5 per segnale in [-2.5, 2.5]).
     %   offset_PRBS  - Offset o livello medio del segnale PRBS (es. 0 per segnale centrato).
+    %   seed (1 o 2) - Mi dice se prendere il PRBS1 o PRBS2, per avere 2 PRBS distinti e scorrelati 
     %
     % Output:
     %   generated_signal_PRBS - Oggetto 'iddata' contenente il segnale PRBS. 
     %                           Contiene i dati del segnale, il nome del canale di input 
     %                           e l'intervallo di campionamento (DeltaT).
 
-    % Genero un segnale PRBS discreto
-    PRBS_bin = idinput(N_sim, 'prbs'); 
+    % Genero un segnale PRBS discreto di 2 N_sim campioni
+    PRBS_bin = idinput(2* N_sim, 'prbs'); 
 
-    %lo adatto in ampiezza, offset e passo di campionamento
+    % In base al seed uso la prima parte o la seconda, scorrelate
+    if(seed == 1)
+        PRBS_bin = PRBS_bin(1:N_sim); % Seleziono la prima parte del PRBS
+    elseif(seed == 2)
+        PRBS_bin = PRBS_bin(N_sim+1 : end);
+    else
+        error('Errore: Seed non valido. Inserire seed = 1 oppure seed = 2 per ottenere sequenze scorrelate.');
+    end
+
+    % Lo adatto in ampiezza, offset e passo di campionamento
     generated_signal_PRBS = iddata([], amp_PRBS * PRBS_bin + offset_PRBS, DeltaT);
 end
--- Matlab/functions/plot_id_and_real_x_teta.m
+++ Matlab/functions/plot_id_and_real_x_teta.m
@@ -1,13 +1,21 @@
 
 % Plot dei risultati simulazione del simulink di validazione
 
-function plot_id_and_real_x_teta(simOut)
+function plot_id_and_real_x_teta(simOut, ss_too)
     %   Genera un grafico a doppio subplot per i dati di simulazione
     %   per il confronto identificato e sistema vero
     %
     %   Input:
     %   simOut - Oggetto Simulink.SimulationOutput (risultato della simulazione).
+    %   ss_too - booleano che indica se devo stampare anche risultati in
+    %            spazio di stato
     
+    
+    % Se ss_too non viene passato, lo imposta a false di default
+    if nargin < 2
+        ss_too = false;
+    end
+
 
     % --- Estrazione dei dati ---
     try
@@ -18,58 +26,90 @@ function plot_id_and_real_x_teta(simOut)
         teta_vero_ts = simOut.get('teta_vero_sim'); 
         teta_vero_data = teta_vero_ts.Data;
      
-        tempo_continuo = teta_vero_ts.Time;  %è lo stesso per entrambi
+        tempo_continuo = teta_vero_ts.Time;
 
 
-        %uscite identificazione
+        %uscite identificazione con modelli polinomiali
         x_id_ts = simOut.get('x_id_sim'); 
         x_id_data = x_id_ts.Data;
     
         teta_id_ts = simOut.get('teta_id_sim'); 
         teta_id_data = teta_id_ts.Data;
 
-        tempo_discreto = teta_id_ts.Time; %è lo stesso per entrambi
-    
+        tempo_discreto = teta_id_ts.Time; 
+
+        % x_id_data e teta_id_data devono essere riportati su 'tempo_continuo' (continuo)
+        x_id_data = interp1(tempo_discreto, x_id_data, tempo_continuo, 'previous');
+        teta_id_data = interp1(tempo_discreto, teta_id_data, tempo_continuo, 'previous');
+
+
+        %uscite identificazione modello in spazio di stato
+        if(ss_too)
+            ss_x_id_ts = simOut.get('ss_x_id_sim'); 
+            ss_x_id_data = ss_x_id_ts.Data;
         
+            ss_teta_id_ts = simOut.get('ss_teta_id_sim'); 
+            ss_teta_id_data = ss_teta_id_ts.Data;
+    
+            ss_tempo_discreto = ss_teta_id_ts.Time; 
+
+            % x_id_data e teta_id_data devono essere riportati su 'tempo_continuo' (continuo)
+            ss_x_id_data = interp1(ss_tempo_discreto, ss_x_id_data, tempo_continuo, 'previous');
+            ss_teta_id_data = interp1(ss_tempo_discreto, ss_teta_id_data, tempo_continuo, 'previous');
+        end
+    
+        tempo = tempo_continuo;     %d'ora in poi sarà il mio tempo di riferimento
+
     catch ME
         error('Errore nell''estrazione dei dati.');
     end
 
-    %le uscite dei sistemi t.discreti non hanno lo stesso tempo
-    %di quelle a tempo continuo, quindi bisogna interpolare
-
-    
-    % x_id_data e teta_id_data devono essere riportati su 'tempo_continuo' (continuo)
-    x_id_data = interp1(tempo_discreto, x_id_data, tempo_continuo, 'previous');
-    teta_id_data = interp1(tempo_discreto, teta_id_data, tempo_continuo, 'previous');
-
-    tempo = tempo_continuo;     %d'ora in poi sarà il mio tempo di riferimento
     
     % --- x_vero vs x_id ---
     figure('Name', 'Posizione vera vs identificazione'); 
     plot(tempo, x_vero_data, 'b-', 'LineWidth', 1.5); hold on;
     plot(tempo, x_id_data,   'r-', 'LineWidth', 1.5); 
+    if(ss_too)
+        hold on;
+        plot(tempo, ss_x_id_data, '-', 'Color',[0.10, 0.73, 0.61],  'LineWidth', 1.5);
+    end
     grid on;
     
     title('Posizione del Carrello ($x$)', 'Interpreter', 'latex', 'FontSize', 14);
     xlabel('Tempo ($s$)', 'Interpreter', 'latex');
     ylabel('Posizione $x$ ($m$)', 'Interpreter', 'latex');
     
-    legend({'$x_{\text{vero}}(t)$', '$x_{\text{id}}(t)$'}, ...
+    if(ss_too)
+        legend({'$x_{\text{vero}}(t)$', '$x_{\text{id,poly}}(t)$', '$x_{\text{id,SS}}(t)$'}, ...
            'Interpreter', 'latex', 'Location', 'best');
+    else
+        legend({'$x_{\text{vero}}(t)$', '$x_{\text{id,poly}}(t)$'}, ...
+           'Interpreter', 'latex', 'Location', 'best');
+    end
+    
     
     
     % --- teta_vero vs teta_id ---
     figure('Name', 'Angolo vero vs identificazione');
     plot(tempo, teta_vero_data, 'b-', 'LineWidth', 1.5); hold on;
     plot(tempo, teta_id_data,   'r-', 'LineWidth', 1.5);
+    if(ss_too)
+        hold on;
+        plot(tempo, ss_teta_id_data, '-', 'Color', [0.10, 0.73, 0.61], 'LineWidth', 1.5);
+    end
     grid on;
     
     title('Posizione Angolare del Pendolo ($\theta$)', 'Interpreter', 'latex', 'FontSize', 14);
     xlabel('Tempo ($s$)', 'Interpreter', 'latex');
     ylabel('Angolo $\theta$ ($rad$)', 'Interpreter', 'latex');
     
-    legend({'$\theta_{\text{vero}}(t)$', '$\theta_{\text{id}}(t)$'}, ...
+    if(ss_too)
+        legend({'$\theta_{\text{vero}}(t)$', '$\theta_{\text{id,poly}}(t)$', '$\theta_{\text{id,SS}}(t)$'}, ...
            'Interpreter', 'latex', 'Location', 'best');
+    else
+        legend({'$\theta_{\text{vero}}(t)$', '$\theta_{\text{id,poly}}(t)$'}, ...
+           'Interpreter', 'latex', 'Location', 'best');
+    end
+    
 
 end
